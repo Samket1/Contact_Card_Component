5005d168b3669ad9ef9bb2140ddff1f9f0e28750
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-08-17 23:54:26 +0300

    y

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,10 +1,12 @@
 function Contact(props) {
     return (
-        <div>
-            <img src={props.pfp} alt={props.name} width="120px" />
-            <h2>Name: {props.name}</h2>
-            <p>Phone: {props.phone}</p>
-            <p>Email: {props.email}</p>
+        <div className="card">
+            <div className="contact-info">
+                <img src={props.pfp} alt={props.name} width="120px" />
+                <h2>Name: {props.name}</h2>
+                <p>Phone: {props.phone}</p>
+                <p>Email: {props.email}</p>
+            </div>
         </div>
     )
 }
