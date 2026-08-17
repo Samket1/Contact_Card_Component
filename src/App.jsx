87ff031ad36b3a1c7e1a152a0c7da600ab4a7385
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-08-17 23:54:19 +0300

    hello

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import Contact from './Contact'
+function App() {
+  return (
+    <div id="center">
+      <h1>Contact List</h1>
+
+      <Contact
+        name="Random Person"
+        phone="09-12-34-56-78"
+        email="randomP@gmail.com"
+        pfp="https://cdn-icons-png.flaticon.com/512/9706/9706577.png"
+      />
+
+      <Contact
+        name="Random Person 2"
+        phone="09-12-34-56-79"
+        email="randomP2@gmail.com"
+        pfp="https://cdn-icons-png.flaticon.com/512/9706/9706577.png"
+      />
+      <Contact
+        name="Random Person 3"
+        phone="09-12-34-56-80"
+        email="randomP3@gmail.com"
+        pfp="https://cdn-icons-png.flaticon.com/512/9706/9706577.png"
+      />
+      <Contact
+        name="Random Person 4"
+        phone="09-12-34-56-81"
+        email="randomP4@gmail.com"
+        pfp="https://cdn-icons-png.flaticon.com/512/9706/9706577.png"
+      />
+    </div>
+  )
+}
+
+export default App
